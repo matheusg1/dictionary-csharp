47f625ffb3aafe9215f11a6347e77e9648980854
matheus.silva <tecmatheusg@hotmail.com>
2022-12-20 13:40:36 -0300

    primeiro commit

---
+++ Candidato.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryCsharp
+{
+    public class Candidato
+    {
+        public Candidato(string nomeComVotos)
+        {
+            var arr = nomeComVotos.Split(",");
+            var nome = arr[0];
+            int votos = int.Parse(arr[1]);
+            
+            Nome = nome;
+            NumeroDeVotos = votos;
+        }
+
+        public string Nome { get; set; }
+        public int NumeroDeVotos { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DictionaryCsharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var path = @"C:\Users\matheus.silva\Documents\ProjetosPessoais\CSharp\DictionaryCsharp\DictionaryCsharp\doc1.csv";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                Dictionary<string, string> registro = new();
+
+                while (!sr.EndOfStream)
+                {
+                    string linha = sr.ReadLine();
+
+                    var arr = linha.Split(",");
+                    var nome = arr[0];
+                    string votos = arr[1];
+
+                    int novoValor;
+
+                    if (!registro.ContainsKey(nome))
+                    {
+                        registro.Add(nome, votos);
+                    }
+                    else
+                    {
+                        novoValor = int.Parse(registro[nome]) + int.Parse(votos);
+                        registro[nome] = novoValor.ToString();
+                    }
+
+                }
+                foreach (KeyValuePair<string, string> item in registro)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
